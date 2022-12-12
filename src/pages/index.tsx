c64d832bc122547de0787041672adbf1f3250535
RicardoMejolaro <ricardo.mejolaro@gmail.com>
2022-12-12 17:11:57 -0300

    create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import Head from "next/head";
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head><title>Inicio | ig.news</title></Head>
-      <h1>
-        Hello World!
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Olá, Seja bem vindo!</span>
+          <h1>Notícias sobre o mundo do <span>React</span></h1>
+          <p>
+            Tenha acesso a todas as publicações <br />
+            <span>por apenas R$ 9,90 por mês.</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Imagem Garota Codando" />
+      </main>
     </>
   )
 }
