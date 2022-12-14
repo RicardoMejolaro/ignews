21054811dd725c7976a7354a211ea97ee6a0ad0c
mejolaro1 <ricardo.mejolaro@smartstaff.com.br>
2022-12-13 21:43:43 -0300

    implemented authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,32 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi';
+import { GoSignIn, GoSignOut } from 'react-icons/go';
 import styles from './styles.module.scss';
 
 export function SingInButton() {
-    const isUserLoggedIn = true;
+    const { data: session, status } = useSession();
 
-    return isUserLoggedIn ? (
-        <button type='button' className={styles.singInButton}>
+    return status === 'authenticated' ? (
+        <button
+            type='button'
+            className={styles.singInButton}
+            title='Sair'
+            onClick={() => signOut()}
+        >
             <FaGithub color='#04d361' />
-            Ricardo Mejolaro
-            <FiX color='#737380' />
+            {session.user?.name}
+            <GoSignOut color='#04d361' />
         </button>
     ) : (
-        <button type='button' className={styles.singInButton}>
+        <button
+            type='button'
+            className={styles.singInButton}
+            title='Entrar com seu Github'
+            onClick={() => signIn('github')}
+        >
             <FaGithub color='#eba417' />
             Entre com o seu Github
+            <GoSignIn color='#eba417' />
         </button>
     )
 }
