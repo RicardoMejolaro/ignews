176db0e3f2f10011cdbcfca962b976f4980c9e99
mejolaro1 <ricardo.mejolaro@smartstaff.com.br>
2022-12-15 13:29:11 -0300

    created checkout stripe with duplicate check and saving strip customer id in fauna db

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,13 +10,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data: session } = useSession();
 
-    const handleSubscribe = () => {
+    const handleSubscribe = async () => {
         if (!session) {
-            signIn('githubId')
+            signIn('githubId');
             return;
         }
 
-        // criação da checkout session
+        try {
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe?.redirectToCheckout({ sessionId });
+
+        } catch (error) {
+            alert(error);
+        }
 
     }
 
